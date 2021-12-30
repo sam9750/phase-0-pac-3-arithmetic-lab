59af1a99d5da979a77edb59e914daf6838b07156
Sam <sam.wadhwa95@gmail.com>
2021-12-30 00:03:24 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(number1,number2) {
+    return number1 + number2; 
+}
+
+function subtract(number1,number2) {
+    return number1 - number2; 
+}
+
+function multiply(number1,number2) {
+    return number1*number2; 
+}
+
+function divide(number1,number2) {
+    return number1/number2; 
+}
+
+function increment(n) {
+    return n+=1;
+}
+
+function decrement(n) {
+    return n-=1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10) 
+
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n,10);
+    
+}
